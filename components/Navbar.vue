e81f4b93b57367eb7883d7705a4d61e6f403703a
wittayasutt <wittayasutt@gmail.com>
2017-10-09 04:13:53 +0700

    first setup

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="http://bulma.io">
+        <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+      </a>
+
+      <button class="button navbar-burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss">
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,34 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'bludisc',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'The video games marketplace.' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+	head: {
+		title: 'bludisc',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'The video games marketplace.'
+			}
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
+	css: ['bulma', '@/assets/css/main.scss'],
+	loading: { color: '#3B8070' },
+	build: {
+		postcss: {
+			plugins: {
+				'postcss-custom-properties': false
+			}
+		},
+		extend(config, ctx) {
+			if (ctx.dev && ctx.isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		}
+	}
 }
